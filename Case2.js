84d850e33c70a95f1a153b4b6682c49eb6f8bbd5
kautsar07 <kautsar07777@gmail.com>
2023-03-01 01:04:44 +0700

    modify case 2

--- Case2.js
+++ Case2.js
@@ -40,27 +40,30 @@ const comments = [
 let x =0
 let y =0
 
-for (let i = 0; i < comments.length; i++) {
-  if (comments[i].commentId && comments[i].replies) {
-    x = x + 1;
+function totalComment (data){
 
-    for (let j = 0; j < comments[i].replies.length; j++) {
-      if (comments[i].replies[i].replies) {
-        x = x + 1;
-
-        for (let k = 0; k < comments[i].replies[i].replies.length; k++) {
-          if (comments[i].replies[i].replies[i].replies) {
-            x = x + 1;
-          } else {
-            y = y + 1;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].commentId && data[i].replies) {
+      x = x + 1;
+  
+      for (let j = 0; j < data[i].replies.length; j++) {
+        if (data[i].replies[i].replies) {
+          x = x + 1;
+  
+          for (let k = 0; k < data[i].replies[i].replies.length; k++) {
+            if (data[i].replies[i].replies[i].replies) {
+              x = x + 1;
+            } else {
+              y = y + 1;
+            }
           }
+        } else {
+          y = y + 1;
         }
-      } else {
-        y = y + 1;
       }
     }
   }
+  let total = x + y
+  console.log("Total komentar adalah" + " " + total);
 }
-let total = x + y
-console.log("Total komentar adalah" + " " + total);
-
+totalComment(comments)

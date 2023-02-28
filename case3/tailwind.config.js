86146bce19b1017b87d67b9cd4876faa8323430f
kautsar07 <kautsar07777@gmail.com>
2023-02-28 11:01:47 +0700

    setup tailwind

--- case3/tailwind.config.js
+++ case3/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { colors: {
+      brand: {
+        bg1: "1c0c01",
+        bg2: "603322",
+        bg3: "FCE5B8",
+        bg4: "FAFAFA",
+      }
+    }},
   },
   plugins: [],
 }
